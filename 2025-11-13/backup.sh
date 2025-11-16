79f1784eeb7d583909f93b25c165793dc85c9b19
James Scott <necr0mstr@gmail.com>
2025-11-16 12:57:03 +0000

    November backup

---
+++ 2025-11-13/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BACKUPDIR="$HOME/git-files/Qidi-Plus4"
+LOGDIR="$HOME/logs"
+if [[ ! -d ${LOGDIR} ]]; then
+	mkdir -p ${LOGDIR}
+fi
+mkdir -p ${BACKUPDIR}/`date +%F`/
+rsync -avHl --exclude 'timelapse/*' --exclude 'gcodes/*' --exclude 'logs/*' $HOME/printer_data/ ${BACKUPDIR}/`date +%F`/ | tee -a $HOME/logs/klipper_backup_`date +%F`.log
+rsync -avHl $HOME/mainsail-config/client.cfg ${BACKUPDIR}/`date +%F`/client.cfg-`date +%F` | tee -a $HOME/logs/klipper_backup_`date +%F`.log
